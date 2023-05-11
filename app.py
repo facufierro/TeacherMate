c7d6d7edb096860f750e74c4195224779c48c604
Facundo Fierro <fierrof_47@hotmail.com>
2023-05-11 19:58:15 +0200

    Added the base implementation of the models

--- app.py
+++ app.py
@@ -23,23 +23,97 @@ class User(db.Model):
         self.password = password
 
 
-class Student(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-
-class Class(db.Model):
+class Area(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     description = db.Column(db.String(100))
-    students = db.relationship('Student', backref='class', lazy=True)
 
     def __init__(self, description):
         self.description = description
+
+
+class Activity (db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    area_id = db.Column(db.Integer, db.ForeignKey('area.id'), nullable=False)
+    area = db.relationship('Area', backref=db.backref('activities', lazy=True))
+
+    def __init__(self, description, area_id):
+        self.description = description
+        self.area_id = area_id
+
+
+class Level(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+
+    def __init__(self, description):
+        self.description = description
+
+
+class Goal(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+
+    def __init__(self, description):
+        self.description = description
+
+
+class Grades(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    lesson_id = db.Column(db.Integer, db.ForeignKey(
+        'lesson.id'), nullable=False)
+    lesson = db.relationship('Lesson', backref=db.backref('grades', lazy=True))
+
+    def __init__(self, description, lesson_id):
+        self.description = description
+        self.lesson_id = lesson_id
+
+
+class Student(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    surname = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, surname, email, phone):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.phone = phone
+
+
+class Lesson(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    date = db.Column(db.String(100))
+    description = db.Column(db.String(100))
+    introduction = db.Column(db.String(100))
+    conclusion = db.Column(db.String(100))
+    notes = db.Column(db.String(100))
+    activity_id = db.Column(db.Integer, db.ForeignKey(
+        'activity.id'), nullable=False)
+    activity = db.relationship(
+        'Activity', backref=db.backref('lessons', lazy=True))
+    level_id = db.Column(db.Integer, db.ForeignKey('level.id'), nullable=False)
+    level = db.relationship('Level', backref=db.backref('lessons', lazy=True))
+    student_id = db.Column(db.Integer, db.ForeignKey(
+        'student.id'), nullable=False)
+    student = db.relationship(
+        'Student', backref=db.backref('lessons', lazy=True))
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=False)
+    goal = db.relationship('Goal', backref=db.backref('lessons', lazy=True))
+
+    def __init__(self, date, description, introduction, conclusion, notes, activity_id, level_id, student_id, goal_id):
+        self.date = date
+        self.description = description
+        self.introduction = introduction
+        self.conclusion = conclusion
+        self.notes = notes
+        self.activity_id = activity_id
+        self.level_id = level_id
+        self.student_id = student_id
+        self.goal_id = goal_id
 
 
 @app.route('/')
